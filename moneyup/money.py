2338879f042a9eca0caa48fe474e5e9d03eb67b4
VoltaicC <connorbass15@gmail.com>
2022-05-17 16:52:20 -0400

    Update money.py

--- moneyup/money.py
+++ moneyup/money.py
@@ -1,10 +1,13 @@
 import time
 
 money = 0.00
-hourlyRate = 17.00
-dailyHours = 8
+hourlyRate = float(input("Enter your hourly rate: "))
+dailyHours = float(input("Enter amount of hours you will work: "))
+
 
 while (money < (hourlyRate * dailyHours)):
     print("{:.2f}".format(money))
     time.sleep(1)
     money = money + hourlyRate/3600
+print("Congrats, you are free")
+input()
