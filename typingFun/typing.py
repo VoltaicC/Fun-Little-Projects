6f8dd825fd4f9e31d54c7f866b58923b60ca8e09
VoltaicC <70348826+VoltaicC@users.noreply.github.com>
2022-09-11 20:50:50 -0400

    Update typing.py

--- typingFun/typing.py
+++ typingFun/typing.py
@@ -11,10 +11,20 @@ def type_string_with_delay(string):
         delay = random.uniform(.03, .1)  
         time.sleep(delay)  
 
+def type_string(string):
+    keyboard.type(string)  
+
+
 for i in range (len(stories.story)):
     print(str(i) + ": " + stories.story[i]["title"])
 prompt = int(input("Which prompt needs to be typed? "))
 prompt = stories.story[prompt]["content"]
+
+speed = int(input("Enter 0 for fast, or anything else for very fast: "))
 time.sleep(2)
-type_string_with_delay(prompt)
+
+if speed == 0:
+    type_string_with_delay(prompt)
+else:
+    type_string(prompt)
 
